eab91f3201d1afd843601f4f5e90b7a010bfe499
TheCrow95T <85043418+TheCrow95T@users.noreply.github.com>
2024-10-03 15:59:27 +0800

    remove build folder due to accident commit

--- build/middleware/headerCheck.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const headerCheck = (req, res, next) => {
-    if (req.headers["api-key"] !== process.env.API_KEY) {
-        return res.status(403).json({ msg: "Insufficient header sent" });
-    }
-    next();
-};
-exports.default = headerCheck;
